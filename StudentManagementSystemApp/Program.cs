ff40cd938ab64b8a916439cdaedf9eb39a3b5a91
Tiana_Chris <amakachukwu215@gmail.com>
2026-03-12 22:55:34 +0100

    Add student data structures and variables

---
+++ StudentManagementSystemApp/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+class StudentManagementSystem
+{
+    static string[] names = new string [10];
+    static int [] ids = new int[10];
+    static double[] grades = new double[10];
+    static int studentCount = 0;
+
+}
